2526eeeb6442f55fc4ee16a09fb6e512fc4dd1c1
Felipe Maia <felipe.viveiros@gmail.com>
2018-11-12 22:29:13 -0300

    'atualizacao'

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -5,23 +5,28 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-
+        this._listaNegocicoes = new ListaNegociacoes();
     }
     
     adiciona(event) {
         event.preventDefault();
         
         let negociacao = this.getNegociacao();
-        console.log(negociacao._data);
-        console.log(DateHelper.dataParaTexto(negociacao._data)); 
         
+        this._listaNegocicoes.adicina(negociacao);
+        console.log(this._listaNegociacoes.negociacoes);
+
+        _limpaFormulario();
+    }
+    
+    _limpaFormulario() {
         this._inputData.value = '';
         this._inputData.focus();
         this._inputQuantidade.value = '';
-        this._inputValor.value = '';
+        this._inputValor.value = 0.0;
     }
-    
-    getNegociacao(){
+
+    _criaNegociacao(){
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade,
